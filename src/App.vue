edc3d949a242cfd08f5eccd4500bd23cbce5b97b
liam <liambaxter01@gmail.com>
2018-04-13 00:23:22 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="leftHero">
+      <HelloWorld/>
+    </div>
+
+    <div class="rightHero">
+      <HelloWorld/>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.leftHero, .rightHero {
+  width: 20%;
+}
+
+.heroList li {
+  display: block;
+}
+
+.leftHero {
+  float: left;
+}
+
+.rightHero {
+  float: right;
+}
+</style>
